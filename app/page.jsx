aa9f036413966ee56524ce083fadb46f70291902
Niraj <nierforprogramming@gmail.com>
2026-03-30 19:48:22 +0545

    Newsletter section completed

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,19 @@
 import Hero from "@/components/hero";
+import Latest from "@/components/latest";
 import Navbar from "@/components/navbar";
+import NewsLetter from "@/components/newsletter";
 
 export default function Home() {
   return (
-    <div className="px-2 2xl:px-40">
-      <Navbar />
-      <Hero />
-    </div>
+    <>
+      <div className="px-2 2xl:px-40">
+        <Navbar />
+        <Hero />
+      </div>
+      <NewsLetter />
+      <div className="px-2 2xl:px-40">
+        <Latest />
+      </div>
+    </>
   );
 }
---
+++ components/newsletter.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const NewsLetter = () => {
+  return (
+    <section className="bg-accent/15 px-2">
+      <div className="container flex flex-col justify-center py-15 max-w-lg mx-auto">
+        <div className="text-center space-y-4">
+          <h2 className="font-display font-bold text-4xl lg:text-5xl">
+            Deliciousness to your inbox
+          </h2>
+          <p>Enjoy weekly hand picked recipes and recommendations</p>
+        </div>
+
+        <div className="space-y-3 my-4 md:flex">
+          <input
+            className="input input-lg border-none outline-none w-full"
+            type="email"
+            placeholder="Email address"
+          />
+          <button className="btn btn-lg btn-accent w-full md:w-fit uppercase text-white">
+            Join
+          </button>
+        </div>
+
+        <div className="text-center">
+          <p>By joining our newsletter you agree to our</p>
+          <p className="underline decoration-accent">Terms and Conditions</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default NewsLetter;
